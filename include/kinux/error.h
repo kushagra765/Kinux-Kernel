cc97938f3a58bbba43c79393cb17cf1b2bb14036
kushagra <74482007+kushagra765@users.noreply.github.com>
2020-12-18 20:23:53 +0530

    Update error.h

--- include/kinux/error.h
+++ include/kinux/error.h
@@ -1,3 +1,5 @@
+/* This file contains some error functions */
+
 #ifndef _ERROR_H
 #define _ERROR_H
 
@@ -6,5 +8,10 @@
 void error(char *error_str, int error_code) {
     printf("Error: %s" "Exit Code: %d", error_str, error_code);
 }
+
+/* Do not call this error unless it's a fatal error! */
+void fatal_error(char *str) {
+    printf("A fatal error has occured: %s", str);
+}    
 
 #endif //_ERROR_H
