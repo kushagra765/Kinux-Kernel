edd89b6a6423606af7f06f759b8410131c21254f
kushagra <74482007+kushagra765@users.noreply.github.com>
2020-12-18 20:17:48 +0530

    Update echfs.c

--- fs/echfs/echfs.c
+++ fs/echfs/echfs.c
@@ -2,9 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <kinux/error.h> /* error.h displays the error and code */
+#include <stdint.h> //to be created
+#include <kinux/error.h> /* error.h displays the error and the error code */
 #include <time.h>
 #include <string.h>
+#include <uuid.h> //to be created
 #include <unistd.h> //to be created
 
 #define BYTES_PER_SECTOR 512
