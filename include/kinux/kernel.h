7b0de70089df36262f1b21487e3a6d3eccc80e05
kushagra <74482007+kushagra765@users.noreply.github.com>
2020-12-20 20:26:35 +0530

    Create kernel.h

---
+++ include/kinux/kernel.h
@@ -0,0 +1,4 @@
+/* This file contains some kernel functions */
+
+void panic(const char *string); //This function is used for kernel panic.
+void printm(const char *string); //This function is used by the kernel to print messages
