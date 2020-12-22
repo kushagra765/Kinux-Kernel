6069ce9843ee06235abc130859d7625480f18256
kushagra <kushagraverma6b@gmail.com>
2020-12-22 08:31:44 +0000

    Updated

--- include/string.h
+++ include/string.h
@@ -1,9 +1,10 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-//This file contains the important string functions
-
-//Comparing two strings
-int strcmp(const char*, const char*);
+int strcmp(const char *str1, const char *str2);
+char *strcpy(char *str1, char *str2);
+char *strcat(char *str1, char *str2);
+char *strlen(const char *str);
+char *strerror(int err_code);
 
 #endif //_STRING_H
