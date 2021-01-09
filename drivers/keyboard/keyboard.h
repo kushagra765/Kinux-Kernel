b3b58a5b74373aadd0bc42fe84eb1b1c85e6ea0b
kushagra <kushagraverma6b@gmail.com>
2021-01-09 14:52:01 +0000

    Added keyboard.h

---
+++ drivers/keyboard/keyboard.h
@@ -0,0 +1,35 @@
+#ifndef _KEYBOARD_H
+#define _KEYBOARD_H
+
+// This file is still in development
+
+/* Numeric Keys */
+#define NUM_1 0x02
+#define NUM_2 0x03
+#define NUM_3 0x04
+#define NUM_4 0x05
+#define NUM_5 0x06
+#define NUM_6 0x07
+#define NUM_7 0x08
+#define NUM_8 0x09
+#define NUM_9 0x0A
+#define NUM_0 0x0B
+
+/* Alphabet Keys */
+#define A 0x1E
+#define B 0x30
+#define C 0x2E
+#define D 0x20
+#define E 0x12
+#define F 0x21
+#define G 0x22
+#define H 0x23
+#define I 0x17
+#define J 0x1B
+#define K 0x25
+#define L 0x26
+#define M 0x32
+#define N 0x31
+#define O 0x18
+
+#endif //_KEYBOARD_H
