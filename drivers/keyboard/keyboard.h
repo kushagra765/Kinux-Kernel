8cf47ece94b17c21242461e93b44f2972b9244c3
kushagra <kushagraverma6b@gmail.com>
2021-01-10 07:55:35 +0000

    Updated keyboard.h

--- drivers/keyboard/keyboard.h
+++ drivers/keyboard/keyboard.h
@@ -3,7 +3,9 @@
 
 // This file is still in development
 
-/* Numeric Keys */
+#define DATA_PORT 0x60
+
+/* Numeric Keys (above alphabet keys) */
 #define NUM_1 0x02
 #define NUM_2 0x03
 #define NUM_3 0x04
@@ -25,11 +27,37 @@
 #define G 0x22
 #define H 0x23
 #define I 0x17
-#define J 0x1B
+#define J 0x24
 #define K 0x25
 #define L 0x26
 #define M 0x32
 #define N 0x31
 #define O 0x18
+#define P 0x19
+#define Q 0x10
+#define R 0x13
+#define S 0x1F
+#define T 0x14
+#define U 0x16
+#define V 0x2F
+#define W 0x11
+#define X 0x2D
+#define Y 0x15
+#define Z 0x2C
+
+/* Function Keys */
+#define F1 0x3B
+#define F2 0x3C
+#define F3 0x3D
+#define F4 0x3E
+#define F5 0x3F
+#define F6 0x40
+#define F7 0x41
+#define F8 0x42
+#define F9 0x43
+#define F10 0x44
+#define F11 0x57
+#define F12 0x58
+
 
 #endif //_KEYBOARD_H
