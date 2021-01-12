545ae64c6d02355291fae869eebf1400182cd2e7
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-12 19:38:28 +0530

    Update time.h

--- include/time.h
+++ include/time.h
@@ -4,9 +4,21 @@
 time_t time(time_t *timer);
 char *ctime(const time_t *timer);
 clock_t clock(void);
-time_t mktime(struct tm *tptr); //tptr means time pointer
+time_t mktime(struct tm *tptr);
 struct tm *gmtime(const time_t *timer);
 struct tm *localtime(const time_t *timer);
-char *asctime(const struct tm *tptr); //tptr means time pointer
+char *asctime(const struct tm *tptr);
+
+struct tm {
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+}
 
 #endif //_TIME_H
