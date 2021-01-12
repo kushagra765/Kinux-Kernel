2616be27b3a01391b93f896dc02d405eaca49e69
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-12 19:52:43 +0530

    Update types.h

--- include/sys/types.h
+++ include/sys/types.h
@@ -1,9 +1,9 @@
-#ifndef _TYPES_H
-#define _TYPES_H
+#ifndef SYS_TYPES_H
+#define SYS_TYPES_H
 
 typedef uint64_t size_t;
 typedef int64_t time_t;
 typedef int64_t clock_t;
 
 
-#endif //_TYPES_H
+#endif //SYS_TYPES_H
