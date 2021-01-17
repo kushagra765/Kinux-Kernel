cff565daf69e8ba5b01fc677e9a04bfbcfc299e7
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-17 20:17:36 +0530

    Update types.h

--- include/sys/types.h
+++ include/sys/types.h
@@ -4,6 +4,8 @@
 typedef uint64_t size_t;
 typedef int64_t time_t;
 typedef int64_t clock_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
 
-
-#endif //SYS_TYPES_H
+#endif //SYS_TYPES_H 
