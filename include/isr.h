e28bd604ef07d3d019a88c25878f8a4ca208a37e
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-19 20:28:01 +0530

    Update isr.h

--- include/isr.h
+++ include/isr.h
@@ -18,6 +18,23 @@
 #define IRQ14 46
 #define IRQ15 47
 
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
 
 void handler_irq();
 
