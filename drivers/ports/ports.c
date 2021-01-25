c8951505f06773223e69ecc9fd72f76156608c47
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-25 11:16:10 +0530

    Update ports.c

--- drivers/ports/ports.c
+++ drivers/ports/ports.c
@@ -1,6 +1,6 @@
 #include "ports.h"
 
-unsigned char byte_in(unsigned short port){
+unsigned char byte_in(unsigned short port) {
 	unsigned char result;
 	//"=a"(result) puts value of rax in result
 	//"d" (port) puts value of port in dx
@@ -12,7 +12,7 @@ unsigned char byte_in(unsigned short port){
 	return result;
 }
 
-void byte_out(unsigned short port, unsigned char data){
+void byte_out(unsigned short port, unsigned char data) {
 	//"out" is the command to write to a port
 	//here nothing is returned, so there is no = symbol
 	//but there are two variables in output space, so we use a comma to seperate them
