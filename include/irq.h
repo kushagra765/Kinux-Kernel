a4f6d8e1e7395bd8be4dc39b5194dac678775dfe
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-27 20:51:11 +0530

    Updates

--- include/irq.h
+++ include/irq.h
@@ -1,5 +1,5 @@
-#ifndef _ISR_H
-#define _ISR_H
+#ifndef _IRQ_H
+#define _IRQ_H
 
 // IRQ
 #define IRQ0 32
@@ -41,43 +41,6 @@ void install_irq_handler(int irq_num, void(*handler)(registers*));
 void install_irq();
 void handler_irq();
 
-// ISR
-extern void isr0();
-extern void isr1();
-extern void isr2();
-extern void isr3();
-extern void isr4();
-extern void isr5();
-extern void isr6();
-extern void isr7();
-extern void isr8();
-extern void isr9();
-extern void isr10();
-extern void isr11();
-extern void isr12();
-extern void isr13();
-extern void isr14();
-extern void isr15();
-extern void isr16();
-extern void isr17();
-extern void isr18();
-extern void isr19();
-extern void isr20();
-extern void isr21();
-extern void isr22();
-extern void isr23();
-extern void isr24();
-extern void isr25();
-extern void isr26();
-extern void isr27();
-extern void isr28();
-extern void isr29();
-extern void isr30();
-extern void isr31();
-
-void install_isr();
-void handler_isr(struct registers *regs);
-
 struct registers {
    unsigned int gs, fs, es, ds;
    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
@@ -85,4 +48,4 @@ struct registers {
    unsigned int eip, cs, eflags, useresp, ss;
 };
 
-#endif //_ISR_H
+#endif //_IRQ_H
