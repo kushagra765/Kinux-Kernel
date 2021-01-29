647b5123f3da1f88fbfcceea7ed3b20e8765d080
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-29 21:02:40 +0530

    Create init.c

---
+++ init/init.c
@@ -0,0 +1,9 @@
+/* init.c
+ * File is in developent
+ */
+
+#include <idt.h>
+#include <gdt.h>
+#include <drivers/video/vga/vga.h>
+#include <drivers/ports/ports.h>
+#include <drivers/keyboard/keyboard.h>
