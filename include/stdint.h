975d786de966dcfeef9c6a673ee3e2931dbe4136
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-01-30 20:12:16 +0530

    Updated : include/stdint.h

--- include/stdint.h
+++ include/stdint.h
@@ -1,10 +1,14 @@
 #ifndef _STDINT_H
 #define _STDINT_H
 
-/* Unsigned Integers */
 typedef uint8_t;
 typedef uint16_t;
 typedef uint32_t;
 typedef uint64_t;
+
+typedef int8_t;
+typedef int16_t;
+typedef int32_t;
+typedef int64_t;
 
 #endif //_STDINT_H
