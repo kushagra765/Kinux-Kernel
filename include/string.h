2b3aa60999cf301ffc812a6fa5b27d747a9eef4c
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-02-02 19:46:13 +0530

    Fixed size_t error in string.h

--- include/string.h
+++ include/string.h
@@ -1,6 +1,8 @@
 #ifndef _STRING_H
 #define _STRING_H
 
+#include <stddef.h>
+
 int strcmp(const char *str1, const char *str2);
 char *strcpy(char *str1, char *str2);
 char *strcat(char *str1, char *str2);
