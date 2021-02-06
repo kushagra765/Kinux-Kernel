e4a7d1f9f36b7f68f6753db12c104d1d8ecbbac2
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-02-06 21:08:11 +0530

    Update time.h

--- include/time.h
+++ include/time.h
@@ -1,13 +1,7 @@
 #ifndef _TIME_H
 #define _TIME_H
 
-time_t time(time_t *timer);
-char *ctime(const time_t *timer);
-clock_t clock(void);
-time_t mktime(struct tm *tptr);
-struct tm *gmtime(const time_t *timer);
-struct tm *localtime(const time_t *timer);
-char *asctime(const struct tm *tptr);
+#include <stddef.h>
 
 struct tm {
   int tm_sec;
@@ -20,5 +14,13 @@ struct tm {
   int tm_yday;
   int tm_isdst;
 }
+
+time_t time(time_t *timer);
+char *ctime(const time_t *timer);
+clock_t clock(void);
+time_t mktime(struct tm *tptr);
+struct tm *gmtime(const time_t *timer);
+struct tm *localtime(const time_t *timer);
+char *asctime(const struct tm *tptr);
 
 #endif //_TIME_H
