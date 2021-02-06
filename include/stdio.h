f10bebc1db06c11fd4c83dcb1f7be71130eeae8f
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-02-06 21:04:27 +0530

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -1,26 +1,15 @@
 #ifndef _STDIO_H
 #define _STDIO_H
 
+/* include/stdio.h
+ * Author - @kushagra765
+*/
+
 int printf(const char *format, ...);
-int fprintf(FILE *stream, const char *format, ...);
-int sprintf(char *string, const char *format, ...);
-int vprintf(FILE *stream, const char *format, va_list arg);
-int vfscanf(FILE *stream, const char *format, va_list arg);
-int fscanf(FILE *stream, const char *format, ...);
-int fgets(char *string, int num, FILE *stream);
-int fgetc(FILE *stream);
-int fputc(int ch, FILE *stream);
-int fputs(const char *string, FILE *stream);
+int scanf(const char *format, ...);
 int putchar(int ch);
 int getchar(void);
-int fputs(const char *string, FILE *stream);
-int fputc(int ch, FILE *stream);
 int puts(const char *string);
-int putc(int ch, FILE *stream);
 void perror(const char *string);
-int feof(FILE *stream);
-int ferror(FILE *stream);
-void rewind(FILE *stream);
-void clearerr(FILE *stream);
 
 #endif //_STDIO_H
