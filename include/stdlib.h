2ca95043b1b3ad5d2e22736af00a653b13875bc2
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-02-06 21:07:27 +0530

    Removed some functions from stdlib.h

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,17 +1,15 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H
 
+#include <stddef.h>
+
 void *malloc(size_t size);
 void *calloc(size_t items, size_t size);
 void *realloc(void *ptr, size_t size);
 void *free(void *ptr);
-void srand(unsigned int seed);
-void rand(void);
 void exit(int status);
-int system(const char *str);
 void abort(void);
 void _exit(int status);
 char *getenv(const char *name);
-
 
 #endif //_STDLIB_H
