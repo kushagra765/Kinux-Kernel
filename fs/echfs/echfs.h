27cc85946e993327d6689046fbad535db05c2715
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-02-11 21:03:02 +0530

    Create echfs.h

---
+++ fs/echfs/echfs.h
@@ -0,0 +1,26 @@
+#ifndef _ECHFS_H
+#define _ECHFS_H
+
+#include <stdint.h>
+
+/* echfs definitions */
+#define BYTES_PER_SECTOR 512
+
+
+typedef struct echfs_entry_t {
+     uint64_t echfs_parent_id;
+     uint8_t type;
+     char file_name[201];
+     uint64_t time_access;
+     uint64_t time_modified;
+     uint64_t time_create;
+     uint32_t permissions;
+     uint32_t file_owner;
+     uint64_t file_payload;
+     uint64_t file_size;
+} __attribute__((packed));
+
+typedef struct echfs_result {
+     char file_name[201];
+     int file_not_found;
+}
